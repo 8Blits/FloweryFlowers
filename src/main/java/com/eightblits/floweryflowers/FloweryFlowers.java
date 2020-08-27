07f4323a62ebce12929b410de1e13fe8a5979e73
8Blits <68965951+8Blits@users.noreply.github.com>
2020-08-26 20:41:28 -0500

    Initial commit

---
+++ src/main/java/com/eightblits/floweryflowers/FloweryFlowers.java
@@ -0,0 +1,26 @@
+package com.eightblits.floweryflowers;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("floweryflowers")
+public class FloweryFlowers
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public FloweryFlowers() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) { }
+
+    private void doClientStuff(final FMLClientSetupEvent event) { }
+}
